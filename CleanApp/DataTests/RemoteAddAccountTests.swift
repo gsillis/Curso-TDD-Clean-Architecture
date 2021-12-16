26714d79a90edad316330c57179fc7033acd0d67
Gabriela Sillis <gabydias47@gmail.com>
2021-12-16 12:03:33 -0300

    move sut creation to a factory method

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -32,19 +32,15 @@ class RemoteAddAccountTests: XCTestCase {
 
     func test_add_should_call_httpClient_with_correct_url() {
         guard let url = URL(string: "http://any-url.com") else { return }
-        let httpPostClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpPostClient: httpPostClientSpy)
-        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password")
-        sut.add(addAccountModel: addAccountModel)
+        let (sut, httpPostClientSpy) = makeSut(url: url)
+        sut.add(addAccountModel: makeAddAccountModel())
 
         XCTAssertEqual(httpPostClientSpy.url, url)
     }
 
     func test_add_should_call_httpClient_with_correct_data() {
-        guard let url = URL(string: "http://any-url.com") else { return }
-        let httpPostClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpPostClient: httpPostClientSpy)
-        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password")
+        let (sut, httpPostClientSpy) = makeSut()
+        let addAccountModel = makeAddAccountModel()
         let encodedData = try? JSONEncoder().encode(addAccountModel)
         sut.add(addAccountModel: addAccountModel)
 
@@ -54,6 +50,17 @@ class RemoteAddAccountTests: XCTestCase {
 
 // MARK: Helpers
 extension RemoteAddAccountTests {
+
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy) {
+        let httpPostClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpPostClient: httpPostClientSpy)
+        return (sut, httpPostClientSpy)
+    }
+
+    func makeAddAccountModel() -> AddAccountModel {
+        return AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password")
+    }
+
     class HttpClientSpy: HttpPostClient {
         fileprivate var url: URL?
         fileprivate var data: Data?
