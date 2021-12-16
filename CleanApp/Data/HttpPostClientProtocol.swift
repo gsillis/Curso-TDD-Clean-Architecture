157ba0039b0adcd1a47197c29ab002fd18e47b6c
Gabriela Sillis <gabydias47@gmail.com>
2021-12-16 12:25:18 -0300

    move production code to their own files

---
+++ CleanApp/Data/HttpPostClientProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClientProtocol.swift
+//  Data
+//
+//  Created by Gabriela Sillis on 16/12/21.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
