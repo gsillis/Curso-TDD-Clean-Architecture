8c6e0df88dcfd382d4f534d2e62ca8b6516439bc
Gabriela Sillis <gabydias47@gmail.com>
2022-01-13 18:55:33 -0300

    feature: test memory leak

--- CleanApp/DataTests/Http/HttpClientSpy.swift
+++ CleanApp/DataTests/Http/HttpClientSpy.swift
@@ -9,21 +9,21 @@ import Foundation
 import Data
 
 class HttpClientSpy: HttpPostClient {
-    fileprivate var urls = [URL]()
-    fileprivate var data: Data?
-    fileprivate var completion: ((Result<Data, HttpError>) -> Void)?
-
+    var urls = [URL]()
+    var data: Data?
+    var completion: ((Result<Data, HttpError>) -> Void)?
+    
     func post(to url: URL, with data: Data?, completion: @escaping (Result<Data, HttpError>) -> Void) {
         urls.append(url)
         self.data = data
         self.completion = completion
     }
-
-    fileprivate func completeWith(error: HttpError) {
+    
+    func completeWith(error: HttpError) {
         completion?(.failure(error))
     }
-
-    fileprivate func completeWith(data: Data) {
+    
+    func completeWith(data: Data) {
         completion?(.success(data))
     }
 }
