efa6442a1e8d76c6b6cbb7b79e904282e74b24a5
Gabriela Sillis <gabydias47@gmail.com>
2022-03-06 17:34:39 -0300

    ensure SignUpPreesenter shows error message if no passworde is provided

--- CleanApp/PresentationLayerTests/SignUpPresenterTests.swift
+++ CleanApp/PresentationLayerTests/SignUpPresenterTests.swift
@@ -18,6 +18,15 @@ class SignUpPresenter {
         if viewModel.name == nil || viewModel.name!.isEmpty {
             let viewModel = AlertViewModel(title: "Erro", message: "O campo Nome é obrigatório")
             alertView.showMessage(viewModel: viewModel)
+        } else if viewModel.email == nil || viewModel.email!.isEmpty {
+            let viewModel = AlertViewModel(title: "Erro", message: "O campo Email é obrigatório")
+            alertView.showMessage(viewModel: viewModel)
+        } else if viewModel.password == nil || viewModel.password!.isEmpty {
+            let viewModel = AlertViewModel(title: "Erro", message: "O campo Senha é obrigatório")
+            alertView.showMessage(viewModel: viewModel)
+        } else if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
+            let viewModel = AlertViewModel(title: "Erro", message: "O campo Confirmar Senha é obrigatório")
+            alertView.showMessage(viewModel: viewModel)
         }
     }
 }
@@ -40,9 +49,8 @@ struct AlertViewModel: Equatable {
 
 class SignUpPresenterTests: XCTestCase {
 
-    func test_sign_up() {
-        let alertViewSpy = AlertViewSpy()
-        let sut = SignUpPresenter(alertView: alertViewSpy)
+    func test_signUp_should_show_error_message_if_name_is_not_provided() {
+        let (sut, alertViewSpy) = makeSut()
         let signUpViewModel = SignUpViewModel(
             email: "any_email",
             password: "any_password",
@@ -55,9 +63,60 @@ class SignUpPresenterTests: XCTestCase {
             message: "O campo Nome é obrigatório")
         )
     }
+    
+    func test_signUp_should_show_error_message_if_email_is_not_provided() {
+        let (sut, alertViewSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(
+            name: "any_name",
+            password: "any_password",
+            passwordConfirmation: "any_password"
+        )
+        sut.signUp(viewModel: signUpViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(
+            title: "Erro",
+            message: "O campo Email é obrigatório")
+        )
+    }
+    
+    func test_signUp_should_show_error_message_if_password_is_not_provided() {
+        let (sut, alertViewSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(
+            name: "any_name",
+            email: "any_email",
+            passwordConfirmation: "any_password"
+        )
+        sut.signUp(viewModel: signUpViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(
+            title: "Erro",
+            message: "O campo Senha é obrigatório")
+        )
+    }
+    
+    func test_signUp_should_show_error_message_if_password_confirmation_is_not_provided() {
+        let (sut, alertViewSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(
+            name: "any_name",
+            email: "any_email",
+            password: "any_password"
+        )
+        sut.signUp(viewModel: signUpViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(
+            title: "Erro",
+            message: "O campo Confirmar Senha é obrigatório")
+        )
+    }
 }
 
 extension SignUpPresenterTests {
+    func makeSut() -> (sut: SignUpPresenter, alerViewSpy: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        return (sut, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
         func showMessage(viewModel: AlertViewModel) {
