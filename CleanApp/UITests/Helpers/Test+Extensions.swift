ba74da5e1bc551c54228ae0fa5105be341fc7e8b
Gabriela Sillis <gabydias47@gmail.com>
2022-04-07 16:36:15 -0300

    move files to folders

---
+++ CleanApp/UITests/Helpers/Test+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Test+Extensions.swift
+//  UITests
+//
+//  Created by Gabriela Sillis on 07/04/22.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach({ action in
+                (target as NSObject).perform(Selector(action))
+            })
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
