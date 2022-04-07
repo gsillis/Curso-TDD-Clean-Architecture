eb0fe76c3560403f12a4019623ad706e7160a729
Gabriela Sillis <gabydias47@gmail.com>
2022-04-07 12:29:17 -0300

    feature: add UI Layer

---
+++ CleanApp/UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Gabriela Sillis on 07/04/22.
+//
+
+import XCTest
+@testable import UI
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
