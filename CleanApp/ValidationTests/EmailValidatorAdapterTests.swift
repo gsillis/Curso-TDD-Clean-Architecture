127e2f1109e6a4fcb97cc2432d41b10cea7af585
Gabriela Sillis <gabydias47@gmail.com>
2022-04-10 15:19:31 -0300

    feature: add validation target

---
+++ CleanApp/ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Gabriela Sillis on 10/04/22.
+//
+
+import XCTest
+@testable import Validation
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+
+}
