c6d301163a6acabe1ec6534a44bab25f7fe21e7c
Gabriela Sillis <gabydias47@gmail.com>
2022-04-10 15:40:05 -0300

    feature: test validation layer

--- CleanApp/ValidationTests/EmailValidatorAdapterTests.swift
+++ CleanApp/ValidationTests/EmailValidatorAdapterTests.swift
@@ -7,9 +7,32 @@
 //
 
 import XCTest
-@testable import Validation
+import PresentationLayer
+import Validation
 
 class EmailValidatorAdapterTests: XCTestCase {
 
+    func test_invalid_emails() {
+        let sut = makeSut()
+        XCTAssertFalse(sut.isValid(email: "gg"))
+        XCTAssertFalse(sut.isValid(email: "gg@"))
+        XCTAssertFalse(sut.isValid(email: "gg@gg"))
+        XCTAssertFalse(sut.isValid(email: "@gg."))
+        XCTAssertFalse(sut.isValid(email: "@gg.com"))
+    }
+    
+    func test_valid_emails() {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "gaby@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "gaby@gmail.com.br"))
+        XCTAssertTrue(sut.isValid(email: "gaby@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "gaby123@hotmail.com."))
+        XCTAssertTrue(sut.isValid(email: "gaby+8778@hotmail.com"))
+    }
+}
 
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
+    }
 }
