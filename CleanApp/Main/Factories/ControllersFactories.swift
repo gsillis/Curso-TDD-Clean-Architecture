417ef794e87cf5cb0022d1a9333fb20bcf0242f3
Gabriela Sillis <gabydias47@gmail.com>
2022-04-12 19:54:24 -0300

    feature: move WeakVarProxy to its own file

--- CleanApp/Main/Factories/ControllersFactories.swift
+++ CleanApp/Main/Factories/ControllersFactories.swift
@@ -28,24 +28,3 @@ class ControllersFactory {
     }
 }
 
-
-class WeakVarProxy<T: AnyObject> {
-    private weak var instance: T?
-    
-    init(instance: T) {
-        self.instance = instance
-    }
-}
-
-extension WeakVarProxy: AlertView where T: AlertView {
-    func showMessage(viewModel: AlertViewModel) {
-        instance?.showMessage(viewModel: viewModel)
-    }
-}
-
-extension WeakVarProxy: LoadingView where T: LoadingView {
-    func show(viewModel: LoadingViewModel) {
-        instance?.show(viewModel: viewModel)
-    }
-}
-
